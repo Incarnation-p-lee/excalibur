041a5c5c3c9dda0f8fcd3e584309e08f2c86ad04
Pan Li <incarnation.p.lee@gmail.com>
2017-07-18 16:12:42 +0800

    Add script for boot disk.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- script/update_image.sh
+++ script/update_image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo losetup /dev/loop0 floppy.img
+sudo losetup /dev/loop0 disk.img
 sudo mount -v -t ext4 /dev/loop0 /mnt
-sudo cp -v bin/kernel /mnt/boot/kernel
-sudo cp -v initrd.img /mnt/boot/initrd
-sudo umount -v /dev/loop0
-sudo losetup -v -d /dev/loop0
+#sudo cp -v bin/kernel /mnt/boot/kernel
+#sudo cp -v initrd.img /mnt/boot/initrd
+#sudo umount -v /dev/loop0
+#sudo losetup -v -d /dev/loop0
 
 # ./configure --enable-debugger --enable-disasm --with-nogui
 # bochs -q -f config/bochsrc.txt
