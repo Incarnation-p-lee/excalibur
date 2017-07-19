93ac12e5d0a34a8737daa66e016dc579b893d36d
Pan Li <incarnation.p.lee@gmail.com>
2017-07-19 20:15:52 +0800

    Build one disk image for bochs.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- script/disk_image_make.sh
+++ script/disk_image_make.sh
@@ -3,7 +3,7 @@
 # fdisk disk.img
 # hexdump disk.img
 sudo losetup -o 1048576 /dev/loop0 disk.img
-sudo mkfs.ext4 /dev/loop0
+sudo mkfs.ext2 /dev/loop0
 sudo mount -o loop /dev/loop0 /mnt
 sudo mkdir -v /mnt/boot
 sudo cp -rv ./config/grub /mnt/boot/
